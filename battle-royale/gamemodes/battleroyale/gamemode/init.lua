7a2658461e041328f0ba1265824f8f205bbf9d6b
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-02-15 09:43:19 -1000

    start on perks serverside

--- battle-royale/gamemodes/battleroyale/gamemode/init.lua
+++ battle-royale/gamemodes/battleroyale/gamemode/init.lua
@@ -7,6 +7,9 @@ game.ConsoleCommand("sbox_weapons 0\n")
 game.ConsoleCommand("sbox_godmode 0\n")
 game.ConsoleCommand("sbox_noclip 0\n")
 
+util.AddNetworkString("BR_OpenPerkMenu")
+util.AddNetworkString("BR_SelectPerk")
+
 function GM:PlayerSpawnObject()
 	return ply:IsAdmin()
 end
@@ -15,4 +18,17 @@ function GM:PlayerSpawn(ply)
 	player_manager.SetPlayerClass(ply, "player_sandbox")
 	ply:StripWeapons()
 	ply:Give("weapon_fists") -- bare hands
+
+	ply:SetNWInt("BR_Perk", PERK_NONE)
+	ply.chose_perk = false
+
+	net.Start("BR_OpenPerkMenu")
+	net.Send(ply)
 end
+
+net.Receive("BR_SelectPerk", function(len, ply)
+	local perk = net.ReadInt(5)
+	if perk <= PERK_MAX and not ply.chose_perk then
+		ply:SetNWInt("BR_Perk", perk)
+	end
+end)
