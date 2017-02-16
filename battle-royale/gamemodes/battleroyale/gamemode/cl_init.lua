d03ddc6d7d6fa997d53f2bf6f57f7b0cb00ae95e
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-02-16 12:25:03 -1000

    Start on perk menu

--- battle-royale/gamemodes/battleroyale/gamemode/cl_init.lua
+++ battle-royale/gamemodes/battleroyale/gamemode/cl_init.lua
@@ -7,3 +7,16 @@ end
 function GM:ContextMenuOpen()
 	return LocalPlayer():IsAdmin()
 end
+
+net.Receive("br_openperkmenu", function(len)
+	local PerkFrame = vgui.Create("DFrame")
+	PerkFrame:SetSize(600, 300)
+	PerkFrame:Center()
+	PerkFrame:SetDraggable(true)
+	PerkFrame:ShowCloseButton(true)
+	PerkFrame:MakePopup()
+	PerkFrame:SetTitle("Inventory")
+	function PerkFrame:Paint(w, h)
+		draw.RoundedBox(4, 0, 0, w, h, Color(0, 0, 0, 200))
+	end
+end)
