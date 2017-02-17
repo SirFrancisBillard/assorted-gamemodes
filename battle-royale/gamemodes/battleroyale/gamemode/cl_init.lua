48da0f32da88a609a01984043995c6ca365db83b
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-02-16 14:39:19 -1000

    More perk menu updates

--- battle-royale/gamemodes/battleroyale/gamemode/cl_init.lua
+++ battle-royale/gamemodes/battleroyale/gamemode/cl_init.lua
@@ -21,14 +21,10 @@ net.Receive("br_openperkmenu", function(len)
 	PerkFrame:SetDraggable(true)
 	PerkFrame:ShowCloseButton(true)
 	PerkFrame:MakePopup()
-	PerkFrame:SetTitle("Inventory")
+	PerkFrame:SetTitle("Choose a perk")
 	function PerkFrame:Paint(w, h)
 		draw.RoundedBox(4, 0, 0, w, h, Color(0, 0, 0, 200))
 	end
-	local PerkName = vgui.Create("DLabel", PerkFrame)
-	PerkName:SetTextColor(color_white)
-	PerkName:SetText(LocalPlayer():Nick())
-	PerkName:Dock(TOP)
 
 	local PerkScroll = vgui.Create("DScrollPanel", PerkFrame)
 	PerkScroll:SetSize(280, 0)
@@ -41,13 +37,13 @@ net.Receive("br_openperkmenu", function(len)
 	-- local PerkModels = {}
 
 	for k, v in SortedPairs(GM.PerkInfo) do
-		if v <= 0 then continue end
 		PerkPanels[#PerkPanels + 1] = vgui.Create("DPanel", PerkScroll)
 		PerkPanels[#PerkPanels]:SetSize(0, 40)
 		PerkPanels[#PerkPanels]:Dock(TOP)
 		PerkPanels[#PerkPanels].Paint = function(self, w, h)
 			draw.RoundedBox(4, 0, 0, w, h, Color(0, 0, 0, 200))
 		end
+
 		PerkButtons[#PerkButtons + 1] = vgui.Create("DButton", PerkPanels[#PerkPanels])
 		PerkButtons[#PerkButtons]:SetSize(260, 40)
 		PerkButtons[#PerkButtons]:Dock(FILL)
@@ -57,11 +53,13 @@ net.Receive("br_openperkmenu", function(len)
 			net.Start("br_selectperk")
 			net.WriteInt(k, 6)
 			net.SendToServer()
+			PerkFrame:Remove()
 		end
+
 		-- PerkModels[#PerkModels + 1] = vgui.Create("ModelImage", PerkButtons[#PerkButtons])
 		-- PerkModels[#PerkModels]:SetSize(40, 40)
 		-- PerkModels[#PerkModels]:Dock(LEFT)
 		-- PerkModels[#PerkModels]:DockMargin(5, 0, 0, 0)
 		-- PerkModels[#PerkModels]:SetModel(v.model)
-	en
+	end
 end)
